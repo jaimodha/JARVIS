6761b5956d36afc1d9214ea5efcf1729257ddd92
Jai Modha <modhajai@gmail.com>
2017-03-13 09:19:09 -0700

    cleanup on the application.properties and the way the properties are read into the application

--- src/main/groovy/com/transcendinsights/config/AuthServerConfiguration.groovy
+++ src/main/groovy/com/transcendinsights/config/AuthServerConfiguration.groovy
@@ -4,13 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
 import org.springframework.jdbc.datasource.DriverManagerDataSource
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain
+import org.springframework.core.env.Environment
 import org.springframework.security.oauth2.provider.token.TokenStore
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore
 
@@ -23,16 +24,14 @@ import javax.sql.DataSource
  */
 @Configuration
 @EnableAuthorizationServer
+@PropertySource('classpath:application.properties')
 class AuthServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
   @Autowired
   AuthenticationManager authenticationManager
 
-  @Value('${service1.client-id}')
-  String service1ClientId
-
-  @Value('${service1.client-secret}')
-  String service1Secret
+  @Autowired
+  Environment environment
 
   @Override
   void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
@@ -47,9 +46,9 @@ class AuthServerConfiguration extends AuthorizationServerConfigurerAdapter {
   @Bean
   public DataSource dataSource() {
     final DriverManagerDataSource dataSource = new DriverManagerDataSource()
-    dataSource.setUrl('jdbc:mysql://localhost:3306/jarvis?useSSL=false')
-    dataSource.setUsername('root')
-    dataSource.setPassword('password')
+    dataSource.setUrl(environment.getProperty('spring.datasource.url'))
+    dataSource.setUsername(environment.getProperty('spring.datasource.username'))
+    dataSource.setPassword(environment.getProperty('spring.datasource.password'))
     dataSource
   }
 
