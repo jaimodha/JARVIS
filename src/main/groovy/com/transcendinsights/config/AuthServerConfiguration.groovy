0b137453af43044ae5387160df274c14e45091fe
Jai Modha <modhajai@gmail.com>
2017-03-13 00:03:07 -0700

    Update the configuration to get the clients from the database

--- src/main/groovy/com/transcendinsights/config/AuthServerConfiguration.groovy
+++ src/main/groovy/com/transcendinsights/config/AuthServerConfiguration.groovy
@@ -2,12 +2,16 @@ package com.transcendinsights.config
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.datasource.DriverManagerDataSource
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
+
+import javax.sql.DataSource
 
 /**
  * @author Jai Modha
@@ -34,10 +38,20 @@ class AuthServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
   @Override
   void configure(ClientDetailsServiceConfigurer clients) {
-    clients.inMemory()
-    .withClient(service1ClientId)
-    .authorizedGrantTypes('authorization_code','refresh_token','password','client_credentials')
-    .scopes('default')
-    .secret(service1Secret)
+//    clients.inMemory()
+//    .withClient(service1ClientId)
+//    .authorizedGrantTypes('authorization_code','refresh_token','password','client_credentials')
+//    .scopes('default')
+//    .secret(service1Secret)
+    clients.jdbc(dataSource())
+  }
+
+  @Bean
+  public DataSource dataSource() {
+    final DriverManagerDataSource dataSource = new DriverManagerDataSource()
+    dataSource.setUrl('jdbc:mysql://localhost:3306/jarvis?useSSL=false')
+    dataSource.setUsername('root')
+    dataSource.setPassword('password')
+    dataSource
   }
 }
